9894ee682bdf1a790efe35c46f40af08f9d800ec
Aries Zhang <zhangfeng0408@qq.com>
2018-08-21 15:40:44 +1000

    Upgraded to .net standard 2.0

---
+++ test/mns-test/CryptoTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Aliyun.MNS.Tests
+{
+    public class CryptoTest
+    {
+        [Fact]
+        public void ExampleTest()
+        {
+            Assert.Equal("", string.Empty);
+        }
+    }
+}
